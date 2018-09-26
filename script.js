9b62088480bbc15c1882410afc8a6787006738ea
Marco <mcg.tampieri@gmail.com>
2018-09-26 16:33:00 +0200

    ex.1 - Calculer l'age

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ var children = 3;
 var wife = "Fiamma"
 var country = "Nepal"
 var job = "web developper"
-
 console.log("Vous serez " + job + " et habiterez au " + country +", et marié a " + wife + " avec " + children + " enfants");
+
+var currentYear = 2018;
+var birthyear = 1978;
+var age = currentYear - birthyear;
+console.log(age)
