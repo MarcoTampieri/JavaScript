175b75bfc435fde0dc7cc40a72d4897f71614427
Marco <mcg.tampieri@gmail.com>
2018-09-27 10:29:59 +0200

    ex.2 - Langues

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
-var children = 3;
-var wife = "Fiamma"
-var country = "Nepal"
-var job = "web developper"
-console.log("Vous serez " + job + " et habiterez au " + country +", et marié a " + wife + " avec " + children + " enfants");
-
-var currentYear = 2018;
-var birthyear = 1978;
-var age = currentYear - birthyear;
-console.log(age)
-
-var myAge = 25;
-var maxAge = 80;
-var food = "pizza";
-var foodPerDay = 2;
-var foodPerYear = 2 * 365;
-var remainingFood = (maxAge - myAge) * foodPerYear;
-console.log("Il vous reste " + remainingFood + " " + food + " avant d'ateindre l'age de " + maxAge + " ans")
-
-var x = 1 + 2;
-var y = 3;
-var z = 4 / 2;
-var q = x * y;
-var result = q + z;
-console.log(result)
+var language = "es";
+var message = "Hola mundo"
+if (language) {
+    console.log(message);
+}
