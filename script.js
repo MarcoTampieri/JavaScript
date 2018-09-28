fea88f588bdad01856c5305c1143ecbfcae5243c
Marco <mcg.tampieri@gmail.com>
2018-09-28 11:36:54 +0200

    ex.4 - Pair et impair

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
-function subtraction (a, b) {
-    return a - b;
+for (let i = 0; i <=20; i++) {
+    if (i % 2 == 0){
+        console.log(i + " est pair");
+    }
+  else {
+        console.log(i + " est impair");
+    } 
+    
 }
-console.log("result of 5-3 = " + subtraction(5, 3));
 
-
-function division (a, b) {
-    return a / b;
-}
-console.log("result of 10/2 = " + division(10, 2));
-
-
-function multiplication (a, b) {
-    return a * b;
-}
-console.log("result of 4*6 = " + multiplication(4, 6));
-
-
-function  percentage (num, per) {
-    return (num/100)*per;
-}
-console.log("result of 38% of 87 = " + percentage(87, 38));
-
-
-function distanceHour(_km, _h) {
-    return _km / _h;
-}
-console.log("average speed of 150km over 2 hours = " + distanceHour(150, 2) + "km/h");
