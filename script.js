ebbe6e07bd8b3fe633c61fe61af39e5e2c5ebe27
Marco <mcg.tampieri@gmail.com>
2018-09-28 15:09:40 +0200

    ex.5 - addition

--- script.js
+++ script.js
@@ -1,76 +1,8 @@
-//EXERCICE 1
+//ESERCIZIO 1
 
-for (let i = 0; i <= 20; i++) {
-    if (i % 2 == 0) {
-        console.log(i + " est pair");
-    }
-    /* else {
-            console.log(i + " est impair");
-        } */
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var result=0;
+for (let i = 0; i < numbers.length; i++) {
+    result += numbers[i];
+    console.log(result);
 }
-
-/*
-for (let i =0; i <= 100; i++) {
-    if (i % 2 == 0) {
-        console.log(i + "even");
-    } else {
-        console.log(i + "not even");
-    }
-} 
-*/
-
-
-
-//EXERCICE 2
-
-let nine = 9;
-for (let i = 0; i <= 10; i++) {
-    let result = nine * i;
-    console.log(i + " * " + nine + " = " + result);
-}
-
-// EXERCICE 3
-
-function grade(score) {
-    if (score > 90) {
-        return "A";
-    } else if (score > 80) {
-        return "B";
-    } else if (score > 70) {
-        return "C";
-    } else if (score > 65) {
-        return "D";
-    } else {
-        return "F"
-    }
-}
-for (i = 0;i <= 100; i++) {
-    console.log("Pour " + i + " points vous avez le grade " + grade(i))
-}
-
-
-// EXERCICE 4
-
-  function halfPiramid (height) {
-    for (let i = 1; i <= height; i++) {
-        let row = "";
-
-        for (let j = 1; j <= i; j++) {
-            row += "*";
-        }
-        console.log(row);
-    }
-}
-halfPiramid(5);
-
-function pyramid(height){
-    for (let i = 0; i < height; i++){
-        let carac = "* ";
-        for (let j = 0; j < i; j++){
-            carac += "* ";
-        }
-        console.log(carac);
-    }
-}
-
-pyramid(8);
