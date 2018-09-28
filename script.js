906ec62efe3a95ca49ce983960c9c95e017472d5
Marco <mcg.tampieri@gmail.com>
2018-09-28 12:27:07 +0200

    ex.4 -  assigner des grades

--- script.js
+++ script.js
@@ -7,8 +7,27 @@ for (let i = 0; i <= 20; i++) {
         } */
 }
 
+
 let nine = 9;
 for (let i = 0; i <= 10; i++) {
     let result = nine * i;
     console.log(i + " * " + nine + " = " + result);
+}
+
+
+function grade(score) {
+    if (score > 90) {
+        return "A";
+    } else if (score > 80) {
+        return "B";
+    } else if (score > 70) {
+        return "C";
+    } else if (score > 65) {
+        return "D";
+    } else {
+        return "F"
+    }
+}
+for (i = 0;i <= 100; i++) {
+    console.log("Pour " + i + " points vous avez le grade " + grade(i))
 }
