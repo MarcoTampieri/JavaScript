7f228c4c31a80540032b27de8b5283cceef37266
Marco <mcg.tampieri@gmail.com>
2018-09-28 11:44:20 +0200

    ex.4 - Multiplication Tables

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-for (let i = 0; i <=20; i++) {
-    if (i % 2 == 0){
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 == 0) {
         console.log(i + " est pair");
     }
-  else {
-        console.log(i + " est impair");
-    } 
-    
+    /* else {
+            console.log(i + " est impair");
+        } */
 }
 
+let nine = 9;
+for (let i = 0; i <= 10; i++) {
+    let result = nine * i;
+    console.log(i + " * " + nine + " = " + result);
+}
