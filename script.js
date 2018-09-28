c5b724f420b684862404cca05fd9347343636fb7
Marco <mcg.tampieri@gmail.com>
2018-09-28 14:13:13 +0200

    ex.4 - pyramide

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//EXERCICE 1
+
 for (let i = 0; i <= 20; i++) {
     if (i % 2 == 0) {
         console.log(i + " est pair");
@@ -7,6 +9,19 @@ for (let i = 0; i <= 20; i++) {
         } */
 }
 
+/*
+for (let i =0; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i + "even");
+    } else {
+        console.log(i + "not even");
+    }
+} 
+*/
+
+
+
+//EXERCICE 2
 
 let nine = 9;
 for (let i = 0; i <= 10; i++) {
@@ -14,6 +29,7 @@ for (let i = 0; i <= 10; i++) {
     console.log(i + " * " + nine + " = " + result);
 }
 
+// EXERCICE 3
 
 function grade(score) {
     if (score > 90) {
@@ -31,3 +47,30 @@ function grade(score) {
 for (i = 0;i <= 100; i++) {
     console.log("Pour " + i + " points vous avez le grade " + grade(i))
 }
+
+
+// EXERCICE 4
+
+  function halfPiramid (height) {
+    for (let i = 1; i <= height; i++) {
+        let row = "";
+
+        for (let j = 1; j <= i; j++) {
+            row += "*";
+        }
+        console.log(row);
+    }
+}
+halfPiramid(5);
+
+function pyramid(height){
+    for (let i = 0; i < height; i++){
+        let carac = "* ";
+        for (let j = 0; j < i; j++){
+            carac += "* ";
+        }
+        console.log(carac);
+    }
+}
+
+pyramid(8);
