5bdd8022a4da8aff76c3f5b6ddd3a49ecc68a568
Marco <mcg.tampieri@gmail.com>
2018-09-28 09:54:10 +0200

    ex.2 code optimisation

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
-var language1 = "es";
-var language2 = "fr";
-var language3 = "en";
-var message1 = "Hola mundo"
-var message2 = "Bonjour tout le monde"
-var message3 = "Hello world"
-if (language1) {
-    console.log(message1);
-} else if (language2) {
-    console.log(message2)
-} else if (language3) {
-    console.log(message3)
+let language = "es";
+if (language == "es") {
+    console.log("Hola mundo");
+} 
+else if (language == "fr") {
+    console.log("Bonjour tout le monde");
+} 
+else if (language == "en") {
+    console.log("Hello world");
+}
+else {
+    console.log("Wrong input");
 }
 
 var score = 87;
-var rankA = "A";
-var rankB = "B";
-var rankC = "C";
-var result = rankA || rankB || rankC;
+var result;
 if (score >= 90) {
-    var result = rankA;
+    result = "A";
 } else if (score >=50) {
-    var result = rankB;
-} else if (score < 50) {
-    var result = rankC;
-};
+    result = "B";
+} else {
+    result = "C";
+}
 console.log(result);
 
-var word = "orange";
+var word = "pomme";
 var wordNumber = 3;
-if (wordNumber <= 1) {
-    var word = "orange";
- } else if (wordNumber >= 2) {
-     var word = "oranges";
- }
- var resultWord = word;
- console.log("I have " + wordNumber + " " + resultWord);
+if (wordNumber > 1) {
+    word += "s";
+}
+ console.log("I have " + wordNumber + " " + word);
