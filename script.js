73244107072da962bf3f4904b449e2cfd3ee7b50
Marco <mcg.tampieri@gmail.com>
2018-09-28 10:02:16 +0200

    ex.3 code optimisation

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-var subtraction = function (a, b) {
+function subtraction (a, b) {
     return a - b;
-};
-var resultSubtraction = subtraction(5, 3);
-console.log("result of 5-3 = " + resultSubtraction);
+}
+console.log("result of 5-3 = " + subtraction(5, 3));
 
-var division = function (a, b) {
+
+function division (a, b) {
     return a / b;
-};
-var resultDivision = division(10, 2)
-console.log("result of 10/2 = " + resultDivision);
+}
+console.log("result of 10/2 = " + division(10, 2));
 
 
-var multiplication = function (a, b) {
+function multiplication (a, b) {
     return a * b;
-};
-var resultMultiplication = multiplication(4, 6);
-console.log("result of 4*6 = " + resultMultiplication);
+}
+console.log("result of 4*6 = " + multiplication(4, 6));
 
-var percentage = function (num, per) {
+
+function  percentage (num, per) {
     return (num/100)*per;
-};
-var resultPercentage = percentage(87, 38);
-console.log("result of 38% of 87 = " + resultPercentage);
+}
+console.log("result of 38% of 87 = " + percentage(87, 38));
 
-var distanceHour = function (_km, _h) {
+
+function distanceHour(_km, _h) {
     return _km / _h;
-};
-var resultDistandeHour = distanceHour(150, 2);
-console.log("average speed of 150km over 2 hours = " + resultDistandeHour + "km/h");
+}
+console.log("average speed of 150km over 2 hours = " + distanceHour(150, 2) + "km/h");
