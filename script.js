b8a22a13670eca32f37f6868b71927c469ece61d
Marco <mcg.tampieri@gmail.com>
2018-10-01 12:31:34 +0200

    ex.7 - PNJ (Personnage non-joueur)

--- script.js
+++ script.js
@@ -1,57 +1,14 @@
-//ESERCIZIO 1
-let arrOne = [7, 5, -12, 6, 32, 14, 18, -2];
-let minOne = Math.min(7, 5, -12, 6, 32, 14, 18, -2);
-console.log("The lowest number is " + minOne);
+//ESERCIZIO1
 
-let arrTwo = [-3, 9, 21, 36, 27, 54, 17, 35];
-let maxTwo = Math.max(-3, 9, 21, 36, 27, 54, 17, 35);
-console.log("The highest number is " + maxTwo);
-
-let result = minOne + maxTwo;
-console.log("The sum is " + result);
-
-//ESERCIZIO 2-1
-
-let floatBateau = 10.4;
-
-let couleBateau = Math.floor(floatBateau);
-console.log("10.4 rounded down is " + couleBateau);
-
-let voleBateau = Math.ceil(floatBateau);
-console.log("10.4 rounded up is " + voleBateau);
-
-//ESERCIZIO 2-2
-
-//ALEATOIRE 50-100
-function randomOne(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-console.log(randomOne(50, 100));
-
-//ALEATOIRE 0 ou 1
-function randomTwo(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+let Character = {
+    name : " Io",
+    age : 25,
+    items : [" rubber duck", "flesh wound", "secrets of the universe"],
+    giveItem: function(){
+        return this.items[Math.floor(Math.random() * this.items.length)];
+    }     
+};
+for (let key in Character) {
+    console.log(key + Character[key]);
 }
-console.log(randomTwo(0, 1));
-
-let randomTwoTwo = Math.random() >= 0.5 ? 1 : 0;
-console.log(randomTwoTwo);
-
-//ALEATOIRE 0 - 10
-function randomThree(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-console.log(randomThree(0, 10));
-
-let randomThreeTwo =Math.random() * (10 - 0 + 1);
-console.log(randomThreeTwo);
-
-//ESERCIZIO 2-3
-
-let arrCharacters = ["Goro", "Johnny Cage", "Kano", "Liu Kang", "Raiden", "Reptile", "Scorpion", "Shang Tsung", "Sonya", "Sub-Zero"]
-let randomCharacter = arrCharacters[Math.floor(Math.random() * arrCharacters.length)];
-console.log(randomCharacter);
+console.log(Character.giveItem())
