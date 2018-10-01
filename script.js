d924e7b60ac8ff0d148ade9f3ecacac16c27a668
Marco <mcg.tampieri@gmail.com>
2018-10-01 16:43:33 +0200

    ex.7 - Bonus, start

--- script.js
+++ script.js
@@ -76,3 +76,36 @@ let mainCharacter = {
 };
 
 mainCharacter.attack();
+
+//ESERCIZIO 4 (BONUS)
+
+let person = {
+    name : "",
+    level : 0,
+    life : 0,
+    weapon : {
+        name: "",
+        damage: 0,
+    },
+    attack: function () {
+        result = this.weapon.damage * this.level;
+    }
+}
+
+ let main = Object.create(person)
+
+ /* const person = {
+    isHuman: false,
+    printIntroduction: function () {
+      console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
+    }
+  };
+  
+  const me = Object.create(person);
+  
+  me.name = "Matthew"; // "name" is a property set on "me", but not on "person"
+  me.isHuman = true; // inherited properties can be overwritten
+  
+  me.printIntroduction();
+  // expected output: "My name is Matthew. Am I human? true"
+  
