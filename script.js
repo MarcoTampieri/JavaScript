28cf2183608b57f08f2fe7419a56326417d7411e
Marco <mcg.tampieri@gmail.com>
2018-10-02 13:36:11 +0200

    ex.7 - BONUS complet

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //ESERCIZIO 1
 
 let Character = {
-    name : " Io",
-    age : 25,
-    items : [" rubber duck", "flesh wound", "secrets of the universe"],
-    giveItem: function(){
+    name: " Io",
+    age: 25,
+    items: [" rubber duck", "flesh wound", "secrets of the universe"],
+    giveItem: function () {
         return this.items[Math.floor(Math.random() * this.items.length)];
-    }     
+    }
 };
 for (let key in Character) {
     console.log(key + Character[key]);
@@ -16,27 +16,27 @@ console.log(Character.giveItem())
 //ESERCIZIO 2
 
 let sword = {
-    title : "Sword",
-    physic : 33,
-    magic : 65,
-    minLevel : 7,
-    available : true,
+    title: "Sword",
+    physic: 33,
+    magic: 65,
+    minLevel: 7,
+    available: true,
 }
 
 let axe = {
-    title : "Axe",
-    physic : 65,
-    magic : 40,
-    minLevel : 10,
-    available : true,
+    title: "Axe",
+    physic: 65,
+    magic: 40,
+    minLevel: 10,
+    available: true,
 }
 
 let baguette = {
-    title : "baguette",
-    physic : 99,
-    magic : 99,
-    minLevel : 20,
-    available : false,
+    title: "baguette",
+    physic: 99,
+    magic: 99,
+    minLevel: 20,
+    available: false,
 }
 
 let toSell = [sword, axe, baguette];
@@ -52,7 +52,7 @@ for (i = 0; i < toSell.length; i++) {
     }
 }
 
-for (i = 0; i <toSell.length; i++) {
+for (i = 0; i < toSell.length; i++) {
     if (toSell[i].minLevel >= 10) {
         console.log("The items of lvl 10+ are: " + toSell[i].title);
     } else {
@@ -65,13 +65,13 @@ let mainCharacter = {
     name: "Io",
     level: 20,
     life: 120,
-    weapon : {
+    weapon: {
         name: "fish",
         damage: 903,
     },
-    attack: function() {
+    attack: function () {
         result = mainCharacter.weapon.damage * mainCharacter.level;
-        console.log(mainCharacter.name + "attacks with " + mainCharacter.weapon.name +" for " + result)
+        console.log(mainCharacter.name + "attacks with " + mainCharacter.weapon.name + " for " + result)
     }
 };
 
@@ -79,33 +79,37 @@ mainCharacter.attack();
 
 //ESERCIZIO 4 (BONUS)
 
-let person = {
-    name : "",
-    level : 0,
-    life : 0,
-    weapon : {
-        name: "",
-        damage: 0,
-    },
-    attack: function () {
-        result = this.weapon.damage * this.level;
-    }
-}
+class Person {
+    constructor(name, level, life, weapon) {
+        this.name = name;
+        this.level = level;
+        this.life = life;
+        this.weapon = weapon = {
+            name: "",
+            damage: 0,
+        };
+    };
+           
+    receiveDamage(result){
+         this.life = this.life - result;
+        console.log(this.name + " now has " + this.life + " remaining HP")
+    };
 
- let main = Object.create(person)
+    attacco(attacked) {
+        console.log(this.name + " attacks " + attacked.name);
+        console.log("with " + this.weapon.name);
+        let result = this.weapon.damage * this.level;
+        console.log("and inflicts " + result + " damage");
+        attacked.receiveDamage(result);
+    };
+};
 
- /* const person = {
-    isHuman: false,
-    printIntroduction: function () {
-      console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
-    }
-  };
-  
-  const me = Object.create(person);
-  
-  me.name = "Matthew"; // "name" is a property set on "me", but not on "person"
-  me.isHuman = true; // inherited properties can be overwritten
-  
-  me.printIntroduction();
-  // expected output: "My name is Matthew. Am I human? true"
-  
+const main = new Person("Bob", 12, 100);
+main.weapon.name = "sword";
+main.weapon.damage = 7;
+
+const enemy = new Person("Carl", 10, 80);
+enemy.weapon.name = "dagger";
+enemy.weapon.damage = 4;
+
+main.attacco(enemy);
