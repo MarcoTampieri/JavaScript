41044da14d668c95f62e920ef2d80c39c7c7cdc8
Marco <mcg.tampieri@gmail.com>
2018-10-08 10:23:24 +0200

    objet bonues life/hunger done

--- Objet Bonus/objetbonus.js
+++ Objet Bonus/objetbonus.js
@@ -1,5 +1,41 @@
+function restoreHealth(character) {
+    character.life = character.life + character.medicine.life - character.food.life;
+    character.hunger = character.hunger + character.medicine.hunger - character.food.hunger;
+}
+
+function restoreLife(character) {
+    character.life += character.medicine.life;
+    character.hunger += character.medicine.hunger;
+}
+
+function restoreHunger(character) {
+    character.life -= character.food.life;
+    character.hunger -= character.food.hunger;
+}
+
+function statusUpdate(character) {
+    if (character.life >= 100 && character.hunger <= 0) {
+        console.log("All good!")
+        character.life -= 3;
+        character.hunger += 3;
+        console.log("Update life : " + character.life + ". Update hunger: " + character.hunger);
+    } else if (character.life < 100 && character.hunger > 0) {
+        restoreHealth(character);
+        console.log("Health Restored: +" + character.medicine.life + "life. -" + character.food.hunger + " hunger.");
+        console.log("Life is " + character.life + ". Hunger is " + character.hunger);
+    } else if (character.life < 100) {
+        restoreLife(character);
+        console.log("Life Restored: +" + character.medicine.life + ". Hunger increased by: " + character.medicine.hunger);
+        console.log("Life is " + character.life + ". Hunger is " + character.hunger);
+    } else if (character.hunger > 0) {
+        restoreHunger(character);
+        console.log("Hunger Restored: -" + character.food.hunger + ". Life decreased by: " + character.food.life);
+        console.log("Hunger is " + character.hunger + ". Life is " + character.life);
+    }
+};
+
 class Person {
-    constructor(name, life, hunger, ) {
+    constructor(name, life, hunger) {
         this.name = name;
         this.life = life;
         this.hunger = hunger;
@@ -12,52 +48,14 @@ class Person {
             hunger: 0,
         };
     };
-    
-    restoreHealth() {
-        this.life = this.life + this.medicine.life + this.food.life;
-        this.hunger = this.hunger + this.medicine.hunger + this.food.hunger;
-    }
-
-    restoreLife() {
-        this.life += this.medicine.life;
-        this.hunger += this.medicine.hunger;
-    }
-
-    resoreHunger() {
-        this.life +=
-    }
-
-    statusUpdate() {
-        if (this.life >= 100 && this.hunger <= 0) {
-            console.log("All good!")
-            this.life -= 3;
-            this.hunger += 3;
-            console.log("Life: " + this.life + "Hunger: " + this.hunger);
-        } else if (this.life < 100 && this.hunger > 0) {
-            restoreHealth()
-            console.log("Health Restored: +" + this.medicine.life + "life. -" + this.food.hunger + " hunger.");
-            console.log("Life is " + this.life + ". Hunger is " + this.hunger);
-        } else if (this.life < 100) {
-            restoreLife()
-            console.log("Life Restored: +" + this.medicine.life + ". Hunger increased by: " + this.medicine.hunger);
-            console.log("Life is " + this.life + ". Hunger is " + this.hunger);
-        } else if (this.hunger > 0) {
-
-        }
-
-
-    };
 };
+
 let hungerRandom = Math.floor(Math.random() * 15) - 1;
 let lifeRandom = Math.floor(Math.random() * 15) + 1;
-const main = new Person("Bob", 11, 0);
-
-main.statusUpdate();
+const main = new Person("Bob", 100, 0);
 main.food.life = -5;
 main.food.hunger = hungerRandom;
 main.medicine.life = lifeRandom;
 main.medicine.hunger = 12;
 
-main.statusUpdate(main);
-
-
+statusUpdate(main);
