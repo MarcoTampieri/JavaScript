d58bd73a5e588c1103ee8343cdb5acd11a58a7c1
Marco <mcg.tampieri@gmail.com>
2018-11-07 09:55:57 +0100

    Ajax exercise, minimalist

---
+++ Ajax/script.js
@@ -0,0 +1,17 @@
+let text = document.getElementById('quote');
+let citazione;
+let xhr = new XMLHttpRequest;
+
+xhr.open('GET', 'https://thatsthespir.it/api', true);
+
+xhr.onload = function () {
+    if (this.status === 200) {
+        citazione = JSON.parse(this.responseText);
+        console.log(citazione);
+    } else {
+        alert("An error occured while communicating with the server.");
+    }
+    text.innerHTML = '"' + citazione.quote + '"\nAuthor: ' + citazione.author + '\n' + '<img src='+citazione.photo+' width=200px height=200px>';
+};
+xhr.send();
+
