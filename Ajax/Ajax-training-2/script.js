d97837f4c5ebe432a14ccee482b17d8c0a9f5f42
Marco <mcg.tampieri@gmail.com>
2018-12-05 16:49:51 +0100

    need to make the cross-search between the inputs

--- Ajax/Ajax-training-2/script.js
+++ Ajax/Ajax-training-2/script.js
@@ -1,16 +1,12 @@
-let global;
-
-let casi = '';
-
 let searchByTitle = document.querySelector(".searchTitle");
 let searchByYear = document.querySelector(".searchYear");
 
 // XXXXXXXXXXXXXXXXXXXXXXXXX SEARCH BY TITLE XXXXXXXXXXXXXXXXXXXXXXXXX
 let postMovieByTitle = (event) => {
 
-    let bob = event.target.value;
+    let searchInput = event.target.value;
     const xhr = new XMLHttpRequest;
-    let url1 = `http://www.omdbapi.com/?apikey=3a3c06ba&s=${bob}`;
+    let url1 = `http://www.omdbapi.com/?apikey=3a3c06ba&s=${searchInput}`;
     xhr.open('GET', url1);
     xhr.send();
     xhr.onreadystatechange = () => {
@@ -23,22 +19,22 @@ let postMovieByTitle = (event) => {
 }
 
 searchByTitle.addEventListener("keyup", function (e) {
-    postMovieByTitle(e);
+    if (e.keyCode === 13) {
+        postMovieByTitle(e);
+    }
 })
 
 //XXXXXXXXXXXXXXXXXXXXXXXXX SEARCH BY YEAR XXXXXXXXXXXXXXXXXXXXXXXXX
 
 let postMovieByYear = (event) => {
     console.log("hello")
-    let bob = event.target.value;
-    
-    let url2 = `http://www.omdbapi.com/?apikey=3a3c06ba&y=${bob}&s=bob`
-    fetch(url2, {
-            method: 'GET'
-        })
+    let searchInput = event.target.value;
+
+    let url2 = `http://www.omdbapi.com/?apikey=3a3c06ba&y=${searchInput}&s=bob`
+    fetch(url2)
         .then((response) => response.json())
+        .catch((error) => console.warn(error))
         .then((parsedData) => postResultList(parsedData))
-        .catch((error) => console.warn(error));
 }
 
 searchByYear.addEventListener("keyup", function (e) {
@@ -49,26 +45,52 @@ searchByYear.addEventListener("keyup", function (e) {
 
 //XXXXXXXXXXXXXXXXXXXXXXXXX UNIVERSAL FUNCTIONS XXXXXXXXXXXXXXXXXXXXXXXXX
 
+// let postResultList = (data) => {
+//     let lista = document.querySelector(".lista");
+//     lista.innerHTML = '';
+//     console.log(data.Search)
+//     for (let i = 0; i < data.Search.length; i++) {
+//         let li = document.createElement("li");
+//         li.id = data.Search[i].imdbID;
+//         //console.log(li.id)
+//         li.innerHTML = /*html*/ `
+//             <a href="showFilm.html?id=${data.Search[i].imdbID}">
+//             <p id="${data.Search[i].imdbID}" class="movieTitle">Title: ${data.Search[i].Title} <br>
+//             Release year: ${data.Search[i].Year} <br>
+//             Type: ${data.Search[i].Type}</p>
+//             </a>
+//         `
+//         lista.appendChild(li);
+
+//     }
+// }
+
 let postResultList = (data) => {
     let lista = document.querySelector(".lista");
     lista.innerHTML = '';
-    for (let i = 0; i < data.Search.length; i++) {
+    data.Search.map(i => {
         let li = document.createElement("li");
-        li.id = data.Search[i].imdbID;
-        console.log(li.id)
-        li.innerHTML = /*html*/ `
-            <p id="${data.Search[i].imdbID}" class="movieTitle">Title: ${data.Search[i].Title} <br>
-            Release year: ${data.Search[i].Year} <br>
-            Type: ${data.Search[i].Type}</p>
+        li.id = i.imdbID;
+        //console.log(li.id)
+        li.innerHTML = /*html*/ ` 
+            <a href="showFilm.html?id=${i.imdbID}">
+            <p id="${i.imdbID}" class="movieTitle">Title: ${i.Title} <br>
+            Release year: ${i.Year} <br>
+            Type: ${i.Type}</p>
+            </a>
         `
         lista.appendChild(li);
-        li.addEventListener("click", showFile)
-    }
-}
 
-let showFile = (data) => {
-    console.log(data.currentTarget.id);
-    let id = data.currentTarget.id;
-    let fileUrl = `http://www.omdbapi.com/?i=${id}&apikey=3a3c06ba`;
-    fetch(fileUrl);
+    })
 }
+// let champ1= document.querySelector("input1");
+// let champ2= document.querySelector("input2");
+// let champ3= document.querySelector("input3");
+
+// formClass.addEventListener("onsubmit", stashValue)
+
+// let stashValue = () => {
+//     let value1= champ1.value
+//     let value2= champ2.value
+//     let value3= champ3.value
+// }
---
+++ Ajax/Ajax-training-2/showFilm.js
@@ -0,0 +1,21 @@
+let url = new URLSearchParams(window.location.search);
+url = url.get('id');
+
+let showFilm = () => {
+    let fileUrl = `http://www.omdbapi.com/?i=${url}&apikey=3a3c06ba`;
+    fetch(fileUrl)
+        .then((response) => response.json())
+        .then((parsedData) => {
+            let filmDiv = document.querySelector(".grid-container");
+            filmDiv.innerHTML = /*html*/ `
+            <div class="poster"><img src="${parsedData.Poster}" alt="Film poster"></div> 
+            <div class="texts">
+             <h3 class=specialH>${parsedData.Title}</h3>
+             <p class="special">Actors <br>${parsedData.Actors}</p> <br>
+             <p class="special">Synopsis <br>${parsedData.Plot}</p>
+             </div>
+             `
+            })
+        }
+
+showFilm();        
